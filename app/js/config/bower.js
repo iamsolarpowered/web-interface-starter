f0a9588b682736e4d8d4a38e73c1062e975787eb
Ben Shymkiw <ben@t.apio.ca>
2016-01-25 19:47:30 -0800

    Shim for angular-route.

--- app/js/config/bower.js
+++ app/js/config/bower.js
@@ -2,5 +2,8 @@ require.config({
   paths: {
     'angular': '../bower_components/angular/angular',
     'angular-route': '../bower_components/angular-route/angular-route'
+  },
+  shim: {
+    'angular-route': ['angular']
   }
 });
