a718658f649b9fe80d56a180ef87e1ceadb94d7a
Ben Shymkiw <ben@t.apio.ca>
2016-01-27 19:43:51 -0800

    Rewrite of main.js.

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,25 @@
 (function() {
-  var magic = {
-    init: function(callback) {
-      require(this.files, this.callback);
-    },
-    callback: function() {
-      angular.element(document).ready(function() {
-        angular.bootstrap(document, ['app']);
-        console.log('✨ Scripts loaded ✨');
-      });
-    },
-    files: [
-      'config/bower',
-      'config/app',
-      'config/routes'
-    ]
-  };
 
-  magic.init();
+  var startTime = new Date();
+
+  var modules = [
+    'config/bower',
+    'config/app',
+    'config/routes'
+  ];
+
+  require(modules, bootstrapAngular);
+
+  function bootstrapAngular(callback) {
+    angular.element(document).ready(function() {
+      angular.bootstrap(document, ['app']);
+      announceSuccess();
+    });
+  }
+
+  function announceSuccess() {
+    var loadTime = (new Date()) - startTime;
+    console.log('✨ Scripts loaded in '+ loadTime +'ms ✨');
+  }
+
 })();
