0a1ae75dca9c6c7dc7ef5f33d91ea16af418868b
Ben Shymkiw <ben@t.apio.ca>
2016-01-27 17:08:10 -0800

    No need for magic in the global namespace.

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,20 @@
-var magic = {
-  init: function(callback) {
-    require(this.files, this.callback);
-  },
-  callback: function() {
-    angular.element(document).ready(function() {
-      angular.bootstrap(document, ['app']);
-      console.log('✨ Scripts loaded ✨');
-    });
-  },
-  files: [
-    'config/bower',
-    'config/app',
-    'routes'
-  ]
-};
+(function() {
+  var magic = {
+    init: function(callback) {
+      require(this.files, this.callback);
+    },
+    callback: function() {
+      angular.element(document).ready(function() {
+        angular.bootstrap(document, ['app']);
+        console.log('✨ Scripts loaded ✨');
+      });
+    },
+    files: [
+      'config/bower',
+      'config/app',
+      'routes'
+    ]
+  };
 
-magic.init();
+  magic.init();
+})();
